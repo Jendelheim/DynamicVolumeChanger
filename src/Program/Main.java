9b2b889f98da86e8d78271f0d576210da6bc64a0
Jendelheim <victor.jendelheim@hotmail.com>
2017-12-08 21:59:51 +0100

    Troligen sista fix-pushen för git.

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,8 +1,10 @@
+package Program;
+
 public class Main {
 
     public static void main (String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("hello world123");
 
     }
 }
